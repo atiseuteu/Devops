e3de1d32a189fa013aecfe3c07c02ec90962b829
Diksha Banekar <diksha.banekar@cumminscollege.in>
2026-01-22 10:16:27 +0530

    Added basic Java Hello World program

---
+++ main.java
@@ -0,0 +1,7 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        System.out.println("My name is Diksha");
+        System.out.println("I am learning Java with Git and GitHub");
+    }
+}
